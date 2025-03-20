bd5450b2dbe03b37fd0a27587610a07a14aaf8e9
Shreekar11 <shreekargade2004@gmail.com>
2025-03-20 20:56:02 +0530

    feat: testing load file added

---
+++ test/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const value = 10000000;
+const main = () => {
+    while (true) {
+        for (let i = 0; i < value; i++) {
+            console.log(i);
+        }
+    }
+};
+main();
---
+++ test/src/index.ts
@@ -0,0 +1,10 @@
+const value = 10000000;
+const main = () => {
+  while (true) {
+    for (let i = 0; i < value; i++) {
+      console.log(i);
+    }
+  }
+};
+
+main();
